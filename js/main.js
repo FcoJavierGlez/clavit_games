2545b38bd28e8d53ee0c17682a1832a5a2a99c03
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-09 10:23:52 +0200

    Updated. Design compatibility with Safari browser

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@
  * @author Francisco Javier González Sabariego
  */
 {    
+    const stinksOfApple = userAgent => /\(.*(?<os>(Mac|iPad|iPod|iPhone|iOS)).*\)/i.test(userAgent) || /Version\/.* Safari/.test(userAgent);
+
     document.addEventListener("DOMContentLoaded", () => {
         const GAMES_CARD = [...document.querySelectorAll(".game-list a")];
 
-        GAMES_CARD.forEach( e => e.addEventListener("click", ev => {
-            ev.preventDefault();
-            e.addEventListener("animationend", () => location.assign(e.href));
-        }));
+        if (!stinksOfApple(navigator.userAgent)) {
+            GAMES_CARD.forEach( e => e.addEventListener("click", ev => {
+                ev.preventDefault();
+                e.addEventListener("animationend", () => location.assign(e.href));
+            }));
+        }
     });
 }
