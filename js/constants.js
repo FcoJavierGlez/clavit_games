f78ef934d6fcb000e324dbdeba8ed322f160fc92
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-09 13:06:53 +0200

    Moved GAMES_LIST from main.js to constants.js

---
+++ js/constants.js
@@ -0,0 +1,17 @@
+const GAMES_LIST = [
+    {
+        name: 'snake',
+        ui_name: 'snake',
+        apple_compatibility: false,
+    },
+    {
+        name: 'seven_and_a_half',
+        ui_name: '7 y media',
+        apple_compatibility: true,
+    },
+    {
+        name: 'simon_says',
+        ui_name: 'simon says',
+        apple_compatibility: true,
+    },
+];
--- js/main.js
+++ js/main.js
@@ -3,24 +3,7 @@
  * 
  * @author Francisco Javier González Sabariego
  */
-{    
-    const GAMES_LIST = [
-        {
-            name: 'snake',
-            ui_name: 'snake',
-            apple_compatibility: false,
-        },
-        {
-            name: 'seven_and_a_half',
-            ui_name: '7 y media',
-            apple_compatibility: true,
-        },
-        {
-            name: 'simon_says',
-            ui_name: 'simon says',
-            apple_compatibility: true,
-        },
-    ]
+{   
     const stinksOfApple = userAgent => /\(.*(?<os>(Mac|iPad|iPod|iPhone|iOS)).*\)/i.test(userAgent) || /Version\/.* Safari/.test(userAgent);
 
     const createCardGame = infoCard => {
