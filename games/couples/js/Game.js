884f8f83a12ac57ad125e9dbc8d03d53400d5c7e
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-15 13:12:35 +0200

    Updated

--- games/couples/js/Game.js
+++ games/couples/js/Game.js
@@ -8,10 +8,28 @@
  class Game {
     
     
-    constructor() {
-        
+    constructor(nCouples) {
+        this.cards = this._createCouples(nCouples);
+        console.log(this.cards);
+    }
+
+    _createCouples = function(nCouples) {
+        if (nCouples % 2 || nCouples < 6) return;
+        let n;
+        let arrayCards = [];
+        for (let i = 0; i < nCouples; i++) {
+            do {
+                n = parseInt( Math.random() * ( nCouples / 2 ) + 1 );
+            } while (arrayCards.filter( e => e.getNumber() == n ).length == 2);
+            arrayCards.push(new Card(n));
+        }
+        return arrayCards;
+    }
+
+    getCards = function() {
+        return this._cards;
     }
 
 
-    
+
 }
--- games/couples/js/main.js
+++ games/couples/js/main.js
@@ -7,6 +7,16 @@
  */
 {
     document.addEventListener("DOMContentLoaded", () => {
-        
+        const CARDS = [...document.getElementsByTagName("div")].filter( e => e.id.match(/\d{1,}/) );
+        const game = new Game(CARDS.length);
+
+        //console.log(CARDS);
+
+        CARDS.forEach( e => {
+            e.addEventListener("click", () => {
+                /* game.touch(e.id - 1); */
+                //console.log(e.id - 1);
+            })
+        })
     });
 }
