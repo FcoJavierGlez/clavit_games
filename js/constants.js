17cde180ad3d36fc9fe4ce3ac0ccdee1e9e12e28
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-21 10:49:32 +0200

    Updated. Added id for each game

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,6 @@
 const GAMES_LIST = [
     {
+        id: 1,
         name: 'snake',
         ui_name: 'snake',
         ios: 1,
@@ -9,6 +10,7 @@ const GAMES_LIST = [
         fechaMod: '2021-03-01'
     },
     {
+        id: 2,
         name: 'seven_and_a_half',
         ui_name: '7 y media',
         ios: 1,
@@ -18,6 +20,7 @@ const GAMES_LIST = [
         fechaMod: '2021-03-01'
     },
     {
+        id: 3,
         name: 'simon_says',
         ui_name: 'simon says',
         ios: 0,
@@ -27,6 +30,7 @@ const GAMES_LIST = [
         fechaMod: '2021-03-01'
     },
     {
+        id: 4,
         name: 'couples',
         ui_name: 'parejas',
         ios: 1,
