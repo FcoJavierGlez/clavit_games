4d7d5cb1e51258c4af5bb73137da2c5e8c3e7b14
FcoJavierGlez <FJGS_86@hotmail.com>
2021-04-22 13:39:28 +0200

    Updated

--- menu_games/js/constants.js
+++ menu_games/js/constants.js
@@ -14,7 +14,7 @@ const GAMES_LIST = [
         name: 'seven_and_a_half',
         ui_name: '7 y media',
         ios: 1,
-        android: 0,
+        android: 1,
         fechaInsercion: '2021-03-01',
         version: "1.0.0",
         fechaMod: '2021-03-01'
@@ -23,7 +23,7 @@ const GAMES_LIST = [
         id: 3,
         name: 'simon_says',
         ui_name: 'simon says',
-        ios: 0,
+        ios: 1,
         android: 1,
         fechaInsercion: '2021-03-01',
         version: "1.0.0",
